d5c7a81c07bb46bf1f6ea5e1a370b13fade0e240
Vrushket <vrushketmore2604@gmail.com>
2023-02-24 10:36:56 +0000

    First commiyt

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            LoginModel obj = new LoginModel();
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Index(LoginModel objuserlogin)
+        {
+            var display = Userloginvalues().Where(m => m.UserName == objuserlogin.UserName && m.UserPassword == objuserlogin.UserPassword).FirstOrDefault();
+            if (display != null)
+            {
+                ViewBag.Status = "CORRECT UserNAme and Password";
+            }
+            else
+            {
+                ViewBag.Status = "INCORRECT UserName or Password";
+            }
+            return View(objuserlogin);
+        }
+        public List<LoginModel> Userloginvalues()
+        {
+            List<LoginModel> objModel = new List<LoginModel>();
+            objModel.Add(new LoginModel { UserName = "user1", UserPassword = "password1" });
+            objModel.Add(new LoginModel { UserName = "user2", UserPassword = "password2" });
+            objModel.Add(new LoginModel { UserName = "user3", UserPassword = "password3" });
+            objModel.Add(new LoginModel { UserName = "user4", UserPassword = "password4" });
+            objModel.Add(new LoginModel { UserName = "user5", UserPassword = "password5" });
+            return objModel;
+        }
+    }
+}
